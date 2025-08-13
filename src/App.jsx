8be962e58b99fd08e072721b3c1b1249ab129172
renukadevi <renukadevi9724@gmail.com>
2025-08-13 13:52:09 +0530

    install tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+       <h1>Book Your Ticket</h1>
+       <p>Select your travel dates and destination</p>
+      </div>
+    </>
+  )
+}
+
+export default App
